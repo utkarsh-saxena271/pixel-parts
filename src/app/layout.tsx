d40e9281aac55bdcc72833b77106c033c4631fb5
utkarsh-saxena271 <utkarshsaxena271@gmail.com>
2026-04-29 22:15:32 +0530

    add some experiments

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -25,9 +25,11 @@ export default function RootLayout({
   return (
     <html
       lang="en"
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
+      className={`${geistSans.variable} ${geistMono.variable} h-full bg-zinc-950 antialiased`}
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className="min-h-full flex flex-col">
+        {children}
+      </body>
     </html>
   );
 }
---
+++ src/components/layout/side-bar.tsx
@@ -0,0 +1,7 @@
+export default function SideBar(){
+    return (
+        <div className="w-60 h-screen overflow-scroll bg-white">
+
+        </div>
+    )
+}
